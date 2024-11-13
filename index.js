148fd85c6c5fa7c9a5ae781c9c07d790c28da568
Cesar-Hernandez-coder <cesar.hernandez25@compscihigh.org>
2024-11-13 15:47:20 +0000

    undone

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 
+
+
+let createPlaylistURL = "";
+let apiKey = ""
+
+
+
+async function createPlaylist(playlistData) {
+const options = {  
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ` + apiKey,
+      'Content-Type':  'application/json',
+    },
+  body: JSON.stringify(playlistData),};
+
+
+const response = await fetch(createPlaylistURL, options);
+
+  const data = await response.json();
+  console.log(data); 
+  return data;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
